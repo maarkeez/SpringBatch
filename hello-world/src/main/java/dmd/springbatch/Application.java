a704d5fdcfdfb6be4660118203c42577d71c13ab
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2018-08-26 01:00:08 +0200

    Hello World - initial commit
    Testing basic features for spring batch

---
+++ hello-world/src/main/java/dmd/springbatch/Application.java
@@ -0,0 +1,35 @@
+package dmd.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class Application {
+
+	public static void main(String[] args) {
+		String[] springConfig = { "jobs/helloWorld.xml" };
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("helloWorldJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Done");
+		context.close();
+	}
+}
